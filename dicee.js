d2f150db1e3387e1ef6050bc7ceb12e1b32f1672
Zahid Hasan <zahidcse98@gmail.com>
2021-09-20 18:13:47 +0600

    Final touches, add who win the game

--- dicee.js
+++ dicee.js
@@ -12,3 +12,18 @@ const player2Num = Math.floor(Math.random() * 6) + 1;
 
 document.getElementById('img1').src = `./images/dice${player1Num}.png`;
 document.getElementById('img2').src = `./images/dice${player2Num}.png`;
+
+let winner;
+
+if (player1Num > player2Num) {
+    winner = player1;
+} else {
+    winner = player2;
+}
+
+let winnerNode = document.createElement('h2');
+winnerNode.textContent = `${winner} won this game`;
+
+let container = document.querySelector('.container');
+
+container.insertBefore(winnerNode, container[1]);
